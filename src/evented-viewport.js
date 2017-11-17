4251ae486d21a6546e36f99a430364d2f8d4fd58
Michael Cavalea <callmecavs@gmail.com>
2017-11-16 20:08:28 -0500

    first take

--- src/evented-viewport.js
+++ src/evented-viewport.js
@@ -1,5 +1,31 @@
-const viewport = () => {
+const viewport = (options = {}) => {
+  const mustard = options.debounce && options.emitter && options.emitter.emit
 
+  if (!mustard) {
+    throw new Error('evented-viewport: Invalid options passed in.')
+  }
+
+  let timeout
+
+  const cache = {}
+
+  const update = () => {
+    cache.width = window.innerWidth
+    cache.height = window.innerHeight
+
+    emitter.emit('resize', cache)
+  }
+
+  const resize = () => {
+    clearTimeout(timeout)
+    timeout = setTimeout(update, options.debounce)
+  }
+
+  update()
+
+  window.addEventListener('resize', resize)
+
+  return cache
 }
 
 export default viewport
